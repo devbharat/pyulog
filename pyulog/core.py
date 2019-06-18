cf3880078d33d2dba9aeb80d5b78b37fc50a584f
devbharat <bharat.tak8@gmail.com>
2019-06-18 14:20:54 +0200

    Parse with CRC if file version crc is set

--- pyulog/core.py
+++ pyulog/core.py
@@ -10,6 +10,74 @@ import numpy as np
 
 __author__ = "Beat Kueng"
 
+# table for calculating CRC
+# this particular table was generated using pycrc v0.7.6, http://www.tty1.net/pycrc/
+# using the configuration:
+#  *    Width        = 16
+#  *    Poly         = 0x1021
+#  *    XorIn        = 0x0000
+#  *    ReflectIn    = False
+#  *    XorOut       = 0x0000
+#  *    ReflectOut   = False
+#  *    Algorithm    = table-driven
+# by following command:
+#   python pycrc.py --model xmodem --algorithm table-driven --generate c
+CRC16_XMODEM_TABLE = [
+        0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+        0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
+        0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+        0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
+        0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
+        0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+        0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
+        0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
+        0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+        0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
+        0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
+        0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+        0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
+        0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
+        0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+        0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
+        0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
+        0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+        0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
+        0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
+        0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+        0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+        0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
+        0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+        0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
+        0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
+        0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+        0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
+        0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
+        0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+        0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
+        0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
+        ]
+
+
+def _crc16(data, crc, table):
+    """Calculate CRC16 using the given table.
+    `data`      - data for calculating CRC, must be bytes
+    `crc`       - initial value
+    `table`     - table for caclulating CRC (list of 256 integers)
+    Return calculated value of CRC
+    """
+    for byte in data:
+        crc = ((crc<<8)&0xff00) ^ table[((crc>>8)&0xff)^byte]
+    return crc & 0xffff
+
+
+def crc16xmodem(data, crc=0):
+    """Calculate CRC-CCITT (XModem) variant of CRC16.
+    `data`      - data for calculating CRC, must be bytes
+    `crc`       - initial value
+    Return calculated value of CRC
+    """
+    return _crc16(data, crc, CRC16_XMODEM_TABLE)
+
 
 # check python version
 if sys.hexversion >= 0x030000F0:
@@ -126,6 +194,7 @@ class ULog(object):
         self._appended_offsets = [] # file offsets for appended data
         self._has_sync = True # set to false when first file search for sync fails
         self._sync_seq_cnt = 0 # number of sync packets found in file
+        self._crc = False
 
         ULog._disable_str_exceptions = disable_str_exceptions
 
@@ -508,7 +577,9 @@ class ULog(object):
             raise Exception("Invalid file format (Failed to parse header)")
         self._file_version, = struct.unpack('B', header_data[7:8])
         if self._file_version > 1:
-            print("Warning: unknown file version. Will attempt to read it anyway")
+            self._crc = True
+            if self._file_version > 2:
+                print("Warning: unknown file version. Will attempt to read it anyway")
 
         # read timestamp
         self._start_timestamp, = ULog._unpack_uint64(header_data[8:])
@@ -516,11 +587,21 @@ class ULog(object):
     def _read_file_definitions(self):
         header = self._MessageHeader()
         while True:
-            data = self._file_handle.read(3)
-            if not data:
+            data_header = self._file_handle.read(3)
+            if not data_header:
                 break
-            header.initialize(data)
+            header.initialize(data_header)
             data = self._file_handle.read(header.msg_size)
+
+            if self._crc:
+                crc_read = self._file_handle.read(2)
+                crc_calc = crc16xmodem(data, crc16xmodem(data_header))
+                if (crc_calc != (crc_read[1] << 8 | crc_read[0])):
+                    if self._debug:
+                        print("CRC match failed _read_file_definitions at 0x%x" % self._file_handle.tell())
+                    # set msg_type to zero to ignore the packet
+                    header.msg_type = 0
+
             if header.msg_type == self.MSG_TYPE_INFO:
                 msg_info = self._MessageInfo(data, header)
                 self._msg_info_dict[msg_info.key] = msg_info.value
@@ -536,11 +617,17 @@ class ULog(object):
             elif (header.msg_type == self.MSG_TYPE_ADD_LOGGED_MSG or
                   header.msg_type == self.MSG_TYPE_LOGGING or
                   header.msg_type == self.MSG_TYPE_LOGGING_TAGGED):
-                self._file_handle.seek(-(3+header.msg_size), 1)
+                if self._crc:
+                    self._file_handle.seek(-(3+header.msg_size+len(crc_read)), 1)
+                else:
+                    self._file_handle.seek(-(3+header.msg_size), 1)
                 break # end of section
             elif header.msg_type == self.MSG_TYPE_FLAG_BITS:
                 # make sure this is the first message in the log
-                if self._file_handle.tell() != 16 + 3 + header.msg_size:
+                offset = 16 + 3 + header.msg_size
+                if self._crc:
+                    offset += len(crc_read)
+                if self._file_handle.tell() != offset:
                     print('Error: FLAGS_BITS message must be first message. Offset:',
                           self._file_handle.tell())
                 msg_flag_bits = self._MessageFlagBits(data, header)
@@ -648,9 +735,9 @@ class ULog(object):
             curr_file_pos = self._file_handle.tell()
 
             while True:
-                data = self._file_handle.read(3)
-                curr_file_pos += len(data)
-                header.initialize(data)
+                data_header = self._file_handle.read(3)
+                curr_file_pos += len(data_header)
+                header.initialize(data_header)
                 data = self._file_handle.read(header.msg_size)
                 curr_file_pos += len(data)
                 if len(data) < header.msg_size:
@@ -661,6 +748,17 @@ class ULog(object):
                         print('read until offset=%i done, current pos=%i' %
                               (read_until, curr_file_pos))
                     break
+
+                if self._crc:
+                    crc_read = self._file_handle.read(2)
+                    curr_file_pos += len(crc_read)
+                    crc_calc = crc16xmodem(data, crc16xmodem(data_header))
+
+                    if (crc_calc != (crc_read[1] << 8 | crc_read[0])):
+                        if self._debug:
+                            print("CRC match failed _read_file_data at 0x%x" % self._file_handle.tell())
+                        # set msg_type to zero to ignore the packet
+                        header.msg_type = 0
 
                 if header.msg_type == self.MSG_TYPE_INFO:
                     msg_info = self._MessageInfo(data, header)
